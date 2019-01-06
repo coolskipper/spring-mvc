bbd087478c52f5383a772e6e6e2457bd6b68faaf
vishal singh <46411932+coolskipper@users.noreply.github.com>
2019-01-06 10:57:27 +0530

    Add files via upload

---
+++ spring-mvc/spring-mvc/src/main/java/cool/skipper/first/springmvc/SpringMvcApplication.java
@@ -0,0 +1,14 @@
+package cool.skipper.first.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcApplication.class, args);
+	}
+
+}
+
