e9d4f1971ffcb846362a0ec7df31c9cffad947f9
visha <vishsal10singh1994@gmail.com>
2019-01-06 12:10:37 +0530

    commit-2

--- spring-mvc/spring-mvc/src/main/java/cool/skipper/first/springmvc/repo/ProductRepository.java
+++ spring-mvc/spring-mvc/src/main/java/cool/skipper/first/springmvc/repo/ProductRepository.java
@@ -1,9 +1,12 @@
 package cool.skipper.first.springmvc.repo;
 
+import javax.persistence.Entity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cool.skipper.first.springmvc.entity.Product;
 
+@Entity
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
